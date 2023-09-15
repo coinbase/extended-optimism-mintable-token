fd80846d73c01fb32c526ae2c35d27beb47f742a
Kanwar <24398939+KNWR@users.noreply.github.com>
2023-09-15 15:33:40 -0400

    Ensure that both `initialize` and `initializeV2` are called on the contract for a totally fresh deploy

--- script/DeployExtendedOptimismMintableToken.s.sol
+++ script/DeployExtendedOptimismMintableToken.s.sol
@@ -59,14 +59,20 @@ contract DeployExtendedOptimismMintableToken is Script {
         console.log("extendedOptimismMintableToken proxy deployed to %s", address(extendedOptimismMintableTokenProxy));
 
         bytes memory initializeCall = abi.encodeWithSelector(
-            extendedOptimismMintableTokenImpl.initializeV2.selector,
+            extendedOptimismMintableTokenImpl.initialize.selector,
             name,
-            symbol,
-            owner
+            symbol
         );
         
         vm.broadcast(admin);
         extendedOptimismMintableTokenProxy.upgradeToAndCall(address(extendedOptimismMintableTokenImpl), initializeCall);
+        // Ensure that contract is properly initialized
+        // 0 is the storage slot for `_initialized` from https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/0a2cb9a445c365870ed7a8ab461b12acf3e27d63/contracts/proxy/utils/Initializable.sol#L62
+        uint64 initializedVersion = uint64(uint(vm.load(
+            address(extendedOptimismMintableToken),
+             0
+        )));
+        require(initializedVersion == 1, "DeployExtendedOptimismMintableToken: initialized version is not 1 after calling `initialize`");
         require(keccak256(abi.encode(extendedOptimismMintableToken.name())) == keccak256(abi.encode(name)), "DeployExtendedOptimismMintableToken: token name incorrect");
         require(keccak256(abi.encode(extendedOptimismMintableToken.symbol())) == keccak256(abi.encode(symbol)), "DeployExtendedOptimismMintableToken: token symbol incorrect");
         require(extendedOptimismMintableToken.BRIDGE() == Predeploys.L2_STANDARD_BRIDGE, 
@@ -84,6 +90,9 @@ contract DeployExtendedOptimismMintableToken is Script {
         vm.prank(address(0));
         require(extendedOptimismMintableTokenProxy.admin() == admin, "DeployExtendedOptimismMintableToken: proxy admin transfer failed");
 
+        vm.broadcast(admin);
+        extendedOptimismMintableToken.initializeV2(name, symbol, owner);
+
         vm.broadcast(owner);
         extendedOptimismMintableToken.grantRole(PAUSER_ROLE, pauser);
         vm.broadcast(owner);
@@ -97,11 +106,11 @@ contract DeployExtendedOptimismMintableToken is Script {
 
         // Ensure that contract is properly initialized
         // 0 is the storage slot for `_initialized` from https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/0a2cb9a445c365870ed7a8ab461b12acf3e27d63/contracts/proxy/utils/Initializable.sol#L62
-        uint64 initializedVersion = uint64(uint(vm.load(
+        uint64 initializedVersion2 = uint64(uint(vm.load(
             address(extendedOptimismMintableToken),
              0
         )));
-        require(initializedVersion == 2, "DeployExtendedOptimismMintableToken: initialized version is not 2");
+        require(initializedVersion2 == 2, "DeployExtendedOptimismMintableToken: initialized version is not 2 after calling `initializev2`");
 
         return address(extendedOptimismMintableToken);
     }
